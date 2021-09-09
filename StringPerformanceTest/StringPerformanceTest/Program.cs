6cd34bf2fdc869e3e8025846db2e621d07d14397
Joe Saxon <jsaxon@glasswallsolutions.com>
2021-09-09 10:10:34 +0100

    feat(test): add project file

---
+++ StringPerformanceTest/StringPerformanceTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace StringPerformanceTest
+{
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        string[] Lines;
+
+        public int NumberOfLines;
+
+        [Params("Bacon", "pork", "prosciutto")]
+        public string SearchValue;
+
+        [Params("Files/Bacon10.txt", "Files/Bacon25.txt", "Files/Bacon50.txt")]
+        public string FileToRead;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            string fileLocation = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), FileToRead);
+            Lines = File.ReadAllLines(fileLocation);
+            NumberOfLines = Lines.Count();
+        }
+
+
+        [Benchmark]
+        public int CountOccurrences()
+        {
+            
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
---
+++ StringPerformanceTest/StringPerformanceTest/README.md
@@ -0,0 +1,10 @@
+﻿This Program reads in lines from a file and counts the occurences of particular words
+
+The Program itself has already been implemented to read in lines from a txt file, however the CountOccurrences method has not yet been implementd. 
+In order to complete this test the CountOccurrences must me implemented that when all lines are read the occurence of each pre-defined word is counted. 
+
+Particular attention should be paid to the time it takes to count the occurences of each word along with memory performance of your implementation.
+
+Following that any choices you make around speed and performance should be noted and justified as to why it was done.
+
+There are a few files of different lengths to allow comparison for time and memory allocation
